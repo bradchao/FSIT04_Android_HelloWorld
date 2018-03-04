448717650c85a6be210ee1d559380ab8f3b4c114
Brad <brad@brad.tw>
2018-03-04 12:04:22 +0800

    201803041204

--- app/src/main/java/tw/org/iii/hello/MainActivity.java
+++ app/src/main/java/tw/org/iii/hello/MainActivity.java
@@ -2,13 +2,24 @@ package tw.org.iii.hello;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textView = findViewById(R.id.lottery);
     }
+
+    public void test2(View view) {
+        textView.setText("Hello, Brad");
+
+    }
+
 }
